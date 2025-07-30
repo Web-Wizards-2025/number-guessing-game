9dc3a056a7b42be52cc78f6b63b67edc5384010c
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2025-07-30 12:28:13 +0200

    Feat: GetPlayerGuess function to receive the user's input created #1

--- index.js
+++ index.js
@@ -1,60 +1,75 @@
 "use strict";
 
-// Functions
+const playerValidGuess = getPlayerGuess();
 
 /*
- 1. Captures the user guess which must be a number between 1 and 100 to be valid
- 2. Checks whether this is valid (guard clause), if not it displays an error message and prompts again the user to take a guess
- 3. If the input is valid then the guess gets saved and returned
+ 1. Gets the player guess
+ 2. Checks for guess validity
+ 3. The user will be repromped to enter a number as long as they keep entering an invalid input
+ 4. When the number is valid it gets returned
  */
-
 function getPlayerGuess() {
-  const playerGuess = prompt(
-    "Guess the secret number. It's an integer between 1 and 100"
-  );
-  const playerGuessNum = Number(playerGuess);
+  let playerGuess;
+  let playerGuessNum;
 
-  const isPlayerGuessValid =
-    playerGuess &&
-    !isNaN(playerGuessNum) &&
-    Number.isInteger(playerGuessNum) &&
-    playerGuessNum > 0 &&
-    playerGuessNum <= 100;
+  // This loop will continue as long as the input is invalid
+  while (true) {
+    playerGuess = prompt(
+      "Guess the secret number. It's an integer between 1 and 100"
+    );
+    playerGuessNum = Number(playerGuess);
 
-  checkGuessValidity(playerGuess, playerGuessNum);
+    // Checks if the input is valid based on all criterias
+    let isGuessValid = checkGuessValidity(playerGuess, playerGuessNum);
 
-  console.log(`Your guess is ${isPlayerGuessValid ? "valid" : "invalid"}`);
-  console.log(`Your guess is ${playerGuessNum}`);
+    // If all checks pass, isValid will still be true, and the loop can be broken
+    if (isGuessValid) break; // Exit the while loop
+    // If isValid is false, the loop will continue, prompting the user again
+  }
+
+  // After the loop, playerGuessNum is guaranteed to be a valid integer between 1 and 100
+  console.log(`You guessed ${playerGuessNum}`);
   return playerGuessNum;
 }
 
-// Checks whether the input is valid and consoles messages depending on the case
+// Checks whether the input is valid
 function checkGuessValidity(input, inputNum) {
-  // Handles a non existent input
-  if (!input)
+  // Handles a non-existent input
+  if (!input) {
     console.log(
       "No input was provided. Please type an integer number between 1 and 100"
     );
+    return false;
+  }
   // Handles an input that results in not a number (a string input)
-  else if (isNaN(inputNum))
+  else if (isNaN(inputNum)) {
     console.log(
       `"${input}" is not a number. Please type an integer number between 1 and 100`
     );
+    return false;
+  }
   // Handles a non-integer number
-  else if (!Number.isInteger(inputNum))
+  else if (!Number.isInteger(inputNum)) {
     console.log(
       `${inputNum} is not a valid number because it's not an integer. Please type an integer number between 1 and 100`
     );
+    return false;
+  }
   // Handles a number less than 1
-  else if (inputNum < 1)
+  else if (inputNum < 1) {
     console.log(
       `${inputNum} is not a valid number because it's less than 1. The number must be an integer between 1 and 100`
     );
+    return false;
+  }
   // Handles a number greater than 100
-  else if (inputNum > 100)
+  else if (inputNum > 100) {
     console.log(
       `${inputNum} is not a valid number because it's greater than 100. The number must be an integer between 1 and 100`
     );
-}
+    return false;
+  }
 
-getPlayerGuess();
+  // Returns true if all checks get passed
+  return true;
+}
