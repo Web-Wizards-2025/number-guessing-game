cefe8210cd4d8177ad464f94e43d53415c134b7f
Gabriel <gabriellugares@gmail.com>
2025-08-01 19:15:06 +0100

    1 - main function of checkGuess

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function checkGuess (playerGuess, answer) {
+    if (playerGuess < answer) {
+        output = `Mwahahaha your guess is too low!`
+    }
+
+    if (playerGuess > answer) {
+        output = `You're making me laugh, your guess is too high!`
+    }
+
+    if (playerGuess === answer) {
+        output = `How did you know...it's not fair! You guessed correctly! The answer was ${answer}.`
+    }
+
+    return output;
+}
